16ccaf33428125281992f26df848fd41f4521f79
MateusF03 <ferrarijuliomateus@gmail.com>
2026-05-06 17:43:42 -0300

    Add GitService enum and update Config to support custom URLs

--- src/main/java/ca/modmonster/minegit/data/Config.java
+++ src/main/java/ca/modmonster/minegit/data/Config.java
@@ -5,6 +5,10 @@ public class Config {
     public String patEncrypted;
     private transient String pat;
 
+    public GitService gitService = GitService.GITHUB;
+    public String customWebUrl = "";
+    public String customApiUrl = "";
+
     public Config(String username, String patEncrypted) {
         this.username = username;
         this.patEncrypted = patEncrypted;
@@ -20,5 +24,34 @@ public class Config {
             if (pat == null) pat = "";
         }
         return pat;
+    }
+
+    public String getWebUrl() {
+        if (gitService.requiresCustomUrl()) {
+            return customWebUrl;
+        }
+        return gitService.getDefaultWebUrl();
+    }
+
+    public String getApiUrl() {
+        if (gitService.requiresCustomUrl()) {
+            return customApiUrl;
+        }
+        return gitService.getDefaultApiUrl();
+    }
+
+    public String buildCloneUrl(String repoName) {
+        if (gitService.requiresCustomUrl()) {
+            // For custom services, assume the repoName already contains the full path
+            // or use the base web URL to construct the clone URL
+            String baseUrl = customWebUrl.endsWith("/") ? customWebUrl : customWebUrl + "/";
+            if (repoName.endsWith(".git")) {
+                return baseUrl + repoName;
+            }
+            return baseUrl + repoName + ".git";
+        }
+
+        String baseUrl = gitService.getDefaultWebUrl();
+        return baseUrl + "/" + username + "/" + repoName + ".git";
     }
 }
---
+++ src/main/java/ca/modmonster/minegit/data/GitService.java
@@ -0,0 +1,33 @@
+package ca.modmonster.minegit.data;
+
+public enum GitService {
+    GITHUB("GitHub", "https://github.com", "https://api.github.com"),
+    GITLAB("GitLab", "https://gitlab.com", "https://gitlab.com/api/v4"),
+    CUSTOM("Custom", null, null); 
+
+    private final String displayName;
+    private final String defaultWebUrl;
+    private final String defaultApiUrl;
+
+    GitService(String displayName, String defaultWebUrl, String defaultApiUrl) {
+        this.displayName = displayName;
+        this.defaultWebUrl = defaultWebUrl;
+        this.defaultApiUrl = defaultApiUrl;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getDefaultWebUrl() {
+        return defaultWebUrl;
+    }
+
+    public String getDefaultApiUrl() {
+        return defaultApiUrl;
+    }
+
+    public boolean requiresCustomUrl() {
+        return this == CUSTOM;
+    }
+}
